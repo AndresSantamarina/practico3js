f8c2991b9973522b62b3d97a0a7259640c52315e
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-07 03:01:38 -0300

    ejercicio 5 del practico 3 resuelto

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,1 +1,13 @@
 //5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+function mostrarInformacion(texto){
+    if (texto === texto.toLowerCase()){
+        document.write("El texto está en minúsculas")
+    }else if (texto === texto.toUpperCase()){
+        document.write("El texto está en mayúsculas")
+    }else{
+        document.write("El texto está formado por una combinación de mayúsculas y minúsculas")
+    }
+}
+
+mostrarInformacion(prompt("Ingrese una cadena de texto"))
