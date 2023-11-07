e0fead6b8ed3cfd3d3f9ff6559a2a86bff3332b9
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-07 02:48:56 -0300

    ejercicio 4 del practico 3 resuelto

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,1 +1,12 @@
 //4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+
+function calcularParImpar (numero){
+    const resultado = numero % 2
+    if (resultado === 0){
+        document.write("El número es par")
+    }else{
+        document.write("El número es impar")
+    }
+}
+
+calcularParImpar(parseInt(prompt("Ingrese un número")));
